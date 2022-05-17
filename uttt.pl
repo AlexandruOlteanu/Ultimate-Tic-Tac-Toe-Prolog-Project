103de3b9d2fc986a4680904e6e98a27e93dc0419
Alexandru <alexandruolteanu2001@gmail.com>
2022-05-17 14:00:43 -0700

    Starting

---
+++ uttt.pl
@@ -0,0 +1,110 @@
+:- dynamic detailed_mode_disabled/0.
+:- ensure_loaded('files.pl').
+% student file for Ultimate Tic Tac Toe implementation
+
+% initialState/1
+% initialState(-State)
+% Este adevărat pentru starea inițială a jocului.
+initialState(_) :- false.
+
+% getBoards/2
+% getBoards(+State, -Boards)
+% Este adevărat dacă în starea State, informațiile din tablele individuale sunt
+% cele din variabila Boards.
+% Boards este legată la o listă de 9 elemente, fiecare element reprezentând o tablă.
+% Ordinea tablelor este cea din lista positions (din utils.pl).
+% Fiecare element din listă este o listă de 9 elemente, reprezentând
+% pozițiile de pe tablă, ca x, 0, sau ''.
+% Pozițiile sunt în ordinea din lista positions (din utils.pl).
+getBoards(_, _) :- false.
+
+% getBoard/3
+% getBoard(+State, +UPos, -Board)
+% Este adebărat dacă în starea State, la poziția UPos din tabla de UTTT, 
+% se află tabla individuală cu reprezentarea din Board.
+% Reprezentarea tablei este descrisă în predicatul getBoards/2.
+getBoard(_, _, _) :- false.
+
+% getUBoard/2
+% getUBoard(stare(+Board, +UboardState, +Player, +NextMoves),
+% -UboardState)
+% Întoarce reprezentarea UBoard-ului, indicând tablele individuale câștigate,
+% remizate, sau încă în desfășurare. Reprezentarea este aceeași ca a tablelor
+% individuale (vezi getBoards/2).
+getUBoard(_, _) :- false.
+
+% getPos/4
+% getPos(+State, +UPos, +Pos, -Cell).
+% Este adevărat dacă în starea State, în tabla individuală de la poziția UPos în UBoard,
+% la poziția Pos pe tablă, se află simbolul Cell (x, 0, sau '').
+getPos(_, _, _, _) :- false.
+
+% getPos/3
+% getPos(+Board, +Pos, -Cell).
+% Este adevărat dacă în tabla individuală reprezentată în Board, la poziția Pos, 
+% se află simbolul Cell (x, 0, sau ''). Predicatul poate fi folosit și pentru UBoard, caz 
+% în care Cell poate fi și r.
+getPos(_, _, _) :- false.
+
+% getNextPlayer/2
+% getNextPlayer(+State), -NextPlayer)
+% Este adevărat dacă în starea State, jucătorul care urmează este NextPlayer
+% (poate fi x sau 0)..
+getNextPlayer(_, _) :- false.
+
+% getNextAvailableBoards/2
+% getNextAvailableBoards(+State, -NextBoardsPoss)
+% Este adevărat dacă în starea State, pozițiile din NextBoardsPoss sunt pozițiile 
+% din UBoard ale tablelor disponibile pentru următoarea mutare.
+getNextAvailableBoards(_, _) :- false.
+
+% getBoardResult/2
+% getBoardResult(+Board, -Result)
+% Este adevărat dacă pentru o tablă individuală (sau UBoard) cu reprezentarea
+% din Board, rezultatul este Result. Result poate fi:
+% x sau 0, dacă jucătorul respectiv a câștigat jocul pe tabla dată;
+% r, dacă s-a ajuns la remiză (toate pozițiile au fost completate dar
+% tabla nu a fost câștigată);
+% '', dacă tabla nu a fost câștigată și nu s-au completat toate pozițiile.
+% NOTĂ: este deja definit predicatul player_wins/2 în utils.pl.
+getBoardResult(_, _) :- false.
+
+% buildState/3
+% buildState(+Boards, +PreviousPos, -State)
+% Este adevărat dacă starea State corespunde stării jocului în care tablele
+% individuale sunt cele din lista Boards, iar ultima mutare a fost în 
+% poziția PreviousPos într-o tablă individuală.
+% NOTĂ: nu contează în care tablă individuală s-a realizat ultima mutare.
+buildState(_, _, _) :- false.
+
+% validMove/2
+% validMove(+State, +Move)
+% Este adevărat dacă mutarea Move este legală în starea State.
+% Move este fie o poziție, în cazul în care este o singură tablă disponibilă
+% pentru a următoarea mutare din starea State, fie o pereche de poziții, altfel.
+validMove(_, _) :- false.
+
+% makeMove/3
+% makeMove(+State, +Move, -NewState)
+% Este adevărat dacă în urma aplicării mutării Move în starea State
+% rezulta starea NewState.
+% Move este fie o poziție (din lista positions), în cazul în care nu sunt mai 
+% multe table disponibile pentru a următoarea mutare din starea State,
+% fie o pereche de poziții, altfel.
+%
+% Hint: folosiți validMove pentru a verifica mutarea și buildState pentru a construi o stare.
+makeMove(_, _, _) :- false.
+
+% dummy_first/2
+% dummy_first(+State, -NextMove)
+% Predicatul leagă NextMove la următoarea mutare pentru starea State.
+% Strategia este foarte simplă: va fi aleasă cea mai din stânga-sus mutare posibilă
+% (prima din lista de poziții disponibile).
+dummy_first(_, _) :- false.
+
+% dummy_last/2
+% dummy_last(+State, -NextMove)
+% Predicatul leagă NextMove la următoarea mutare pentru starea State.
+% Strategia este foarte simplă: va fi aleasă cea mai din dreapta-jos mutare posibilă 
+% (ultima din lista de poziții disponibile).
+dummy_last(_, _) :- false.
